2d749cd6b988f682cf61ef734044e9c45e75831f
karasu yami <demigodlinty@gmail.com>
2022-12-21 15:08:10 -0600

    added all api routes headed towards module

--- 02-Challenge/Develop/routes/api/tag-routes.js
+++ 02-Challenge/Develop/routes/api/tag-routes.js
@@ -1,28 +1,93 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const { rawAttributes } = require('../../models/Product');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+
+  Tag.findAll({
+    include:{
+      model: Product,
+     attributes: ['catagory_id', 'price', 'stock']
+    }
+  }).then((TagDb) => {
+    res.json(TagDb);
+  }).catch(err =>{
+    console.log(err)
+    res.status(500).json(err);
+  });
+ 
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where:{
+      id: req.params.id
+    },
+    include:{
+      model: Product,
+     attributes: ['catagory_id', 'price', 'stock']
+    }
+  }).then((TagDb) => {
+    res.json(TagDb)
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  }).then(TagDb => {
+    res.json(TagDb)
+  }).catch(err => {
+    console.log(err)
+    res.status(500).res.json(err)
+  })
+  
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where:{
+      id: req.params.id
+    }
+ }).then(TagDb =>{
+  if(!TagDb){
+    res.status(404).json({message:'no such tag id exist'})
+    return;
+  }
+  res.json(TagDb);
+ }).catch(err => {
+  console.log(err)
+  res.status(500).res.json(err)
+})
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+      id: req.params.id
+    }
+  }).then(TagDb =>{
+    if(!TagDb){
+      res.status(404).json({message:'no such tag id exist'})
+      return;
+    }
+    res.json(TagDb);
+   }).catch(err => {
+    console.log(err)
+    res.status(500).res.json(err)
+  });
 });
 
 module.exports = router;
